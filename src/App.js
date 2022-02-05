7adec5ea9dc1a11f7712afae7a3e3d9a96f09766
Troy Stocks <tstocks@ceratopsdev.com>
2022-02-05 17:51:10 -0500

    trigger build

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>I was changed!</p>
+        <p>I was changed to trigger a build!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
